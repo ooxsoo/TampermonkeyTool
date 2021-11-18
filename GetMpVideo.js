e42a581008c524b4b6ed098aed715bb24a719cd2
xs <23733834+ooxsoo@users.noreply.github.com>
2021-11-18 11:00:53 +0800

    change run-at

--- GetMpVideo.js
+++ GetMpVideo.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         GetMpVideo
 // @namespace    https://github.com/ooxsoo/TampermonkeyTool
-// @version      0.1
+// @version      0.1.1
 // @description  get wechat mp video
 // @author       ooxsoo
 // @match        https://mp.weixin.qq.com/s/*
 // @icon         https://www.google.com/s2/favicons?domain=qq.com
 // @grant        none
-// @run-at       document-body
+// @run-at       document-idle
 // @updateURL    https://ooxsoo.github.io/TampermonkeyTool/GetMpVideo.js
 // ==/UserScript==
 
