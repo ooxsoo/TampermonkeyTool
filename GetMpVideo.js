7a4655294452a65d1f7f325ea27dff8f20ff6ab5
xs <23733834+ooxsoo@users.noreply.github.com>
2021-11-18 10:54:06 +0800

    get wechat mp video

---
+++ GetMpVideo.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         GetMpVideo
+// @namespace    https://github.com/ooxsoo/TampermonkeyTool
+// @version      0.1
+// @description  get wechat mp video
+// @author       ooxsoo
+// @match        https://mp.weixin.qq.com/s/*
+// @icon         https://www.google.com/s2/favicons?domain=qq.com
+// @grant        none
+// @run-at       document-body
+// @updateURL    https://ooxsoo.github.io/TampermonkeyTool/GetMpVideo.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  let arr = window.__mpVideoTransInfo;
+  if (arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let data = arr[i];
+      console.log(data.video_quality_wording + ":" + data.url);
+    }
+  }
+})();
